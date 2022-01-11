148bbbf398091b77ba99757250315f8fc973622d
Alekususenpai <83718750+Alekususenpai@users.noreply.github.com>
2022-01-12 00:56:31 +0100

    Boards CSS base added

--- workboard/src/components/BoardOne/BoardOne.js
+++ workboard/src/components/BoardOne/BoardOne.js
@@ -3,28 +3,28 @@ import './BoardOne.css';
 
 const BoardOne = () => {
           return (
-                    <div class="board" id="one">
+                    <div className="board" id="one">
 
-                              <div class="card">
-                                        <div class="big-year" id="yearNum">
+                              <div className="card">
+                                        <div className="big-year" id="yearNum">
                                         </div>
-                                        <span class="triangle-left">
+                                        <span className="triangle-left">
                                         </span>
-                                        <span class="triangle-right">
+                                        <span className="triangle-right">
                                         </span>
-                                        <div class="months">
+                                        <div className="months">
                                         </div>
-                                        <hr class="month-line" />
-                                        <table class="calendar-table" id="calendar">
+                                        <hr className="month-line" />
+                                        <table className="calendar-table" id="calendar">
                                                   <thead>
                                                             <tr>
-                                                                      <th class="days-of-week">Sun</th>
-                                                                      <th class="days-of-week">Mon</th>
-                                                                      <th class="days-of-week">Tue</th>
-                                                                      <th class="days-of-week">Wed</th>
-                                                                      <th class="days-of-week">Thu</th>
-                                                                      <th class="days-of-week">Fri</th>
-                                                                      <th class="days-of-week">Sat</th>
+                                                                      <th className="days-of-week">Sun</th>
+                                                                      <th className="days-of-week">Mon</th>
+                                                                      <th className="days-of-week">Tue</th>
+                                                                      <th className="days-of-week">Wed</th>
+                                                                      <th className="days-of-week">Thu</th>
+                                                                      <th className="days-of-week">Fri</th>
+                                                                      <th className="days-of-week">Sat</th>
                                                             </tr>
                                                   </thead>
                                                   <tbody id="calendar-body">
@@ -32,10 +32,10 @@ const BoardOne = () => {
                                         </table>
                               </div>
 
-                              <div class="quote">
+                              <div className="quote">
                                         <h2>Little bit of motivation</h2>
-                                        <div id="message" class="message"></div>
-                                        <div class="btndiv"><button id="messageBtn" class="btn">Hit me!</button></div>
+                                        <div id="message" className="message"></div>
+                                        <div className="btndiv"><button id="messageBtn" className="btn">Hit me!</button></div>
                               </div>
 
                     </div>
