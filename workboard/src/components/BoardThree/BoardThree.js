aec71bc40af401bce792c88168ebcc07d9b4cd67
Alekususenpai <83718750+Alekususenpai@users.noreply.github.com>
2022-01-12 00:55:31 +0100

    Board Three foundation

--- workboard/src/components/BoardThree/BoardThree.js
+++ workboard/src/components/BoardThree/BoardThree.js
@@ -3,7 +3,33 @@ import './BoardThree.css';
 
 const BoardThree = () => {
           return (
-                    <div>
+                    <div className="board" id="three">
+                              <div className="main">
+                                        <h2>TO-DO LIST</h2>
+                                        <form className="" action="index.html" method="post">
+                                                  <input type="text" placeholder="Add a new task" name="task"></input>
+                                                  <button type="submit">ADD</button>
+                                        </form>
+                                        <div className="tasksBoard">
+                                                  <ul>
+                                                            <li>
+                                                                      <span className="delete">x</span>
+                                                                      <input type="checkbox"></input>
+                                                                      <label>Read one chapter</label>
+                                                            </li>
+                                                            <li><span className="delete">x</span><input
+                                                                      type="checkbox"></input><label>Update
+                                                                                project</label>
+                                                            </li>
+                                                            <li><span className="delete">x</span><input
+                                                                      type="checkbox"></input><label>Write a
+                                                                                mail</label>
+                                                            </li>
+
+                                                  </ul>
+                                                  <button id="clear">Clear</button>
+                                        </div>
+                              </div>
 
                     </div>
           )
